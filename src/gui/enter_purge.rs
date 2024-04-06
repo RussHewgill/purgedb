1f7bb3e7bceffd478fea8c7bef05a54197d62fd4
RussHewgill <RussHewgill@users.noreply.github.com>
2024-04-05 18:53:49 -0700

    added offset and multiplier

--- src/gui/enter_purge.rs
+++ src/gui/enter_purge.rs
@@ -3,104 +3,144 @@ use crate::types::Filament;
 
 #[derive(Debug, Default, serde::Serialize, serde::Deserialize)]
 pub struct EnterPurge {
-  // filament1: Filament,
-  // filament2: Filament,
-  picker1: FilamentPicker,
-  picker2: FilamentPicker,
-  prev1: Option<Filament>,
-  prev2: Option<Filament>,
-  purge1: String,
-  purge2: String,
+    // filament1: Filament,
+    // filament2: Filament,
+    picker1: FilamentPicker,
+    picker2: FilamentPicker,
+    prev1: Option<Filament>,
+    prev2: Option<Filament>,
+    purge1: String,
+    purge2: String,
 }
 
 impl App {
-  pub fn show_enter_purge(&mut self, ui: &mut egui::Ui) {
-    egui::Frame::none()
-      // .stroke(Stroke::new(1.0, egui::Color32::from_gray(65)))
-      // .outer_margin(5.)
-      // .inner_margin(5.)
-      .show(ui, |ui| {
-        let filaments = self.db.get_all_filaments().unwrap();
-        let resp1 = self
-          .enter_purge
-          .picker1
-          .filament_picker(None, &filaments, ui);
-        let resp2 = self
-          .enter_purge
-          .picker2
-          .filament_picker(None, &filaments, ui);
+    pub fn show_enter_purge(&mut self, ui: &mut egui::Ui) {
+        egui::Frame::none()
+            // .stroke(Stroke::new(1.0, egui::Color32::from_gray(65)))
+            // .outer_margin(5.)
+            // .inner_margin(5.)
+            .show(ui, |ui| {
+                let filaments = self.db.get_all_filaments().unwrap();
+                let resp1 = self
+                    .enter_purge
+                    .picker1
+                    .filament_picker(None, &filaments, ui);
+                let resp2 = self
+                    .enter_purge
+                    .picker2
+                    .filament_picker(None, &filaments, ui);
 
-        if self.enter_purge.picker1.selected != self.enter_purge.prev1 {
-          self.enter_purge.prev1 = self.enter_purge.picker1.selected.clone();
-          self.enter_purge.purge1.clear();
-          self.enter_purge.purge2.clear();
-        }
-        if self.enter_purge.picker2.selected != self.enter_purge.prev2 {
-          self.enter_purge.prev2 = self.enter_purge.picker2.selected.clone();
-          self.enter_purge.purge1.clear();
-          self.enter_purge.purge2.clear();
-        }
-
-        // if resp1.changed() || resp2.changed() {
-        //   self.enter_purge.purge1.clear();
-        //   self.enter_purge.purge2.clear();
-        // }
-
-        ui.separator();
-
-        match (
-          &self.enter_purge.picker1.selected,
-          &self.enter_purge.picker2.selected,
-        ) {
-          (Some(f1), Some(f2)) => {
-            if f1 == f2 {
-              return;
-            }
-            ui.horizontal(|ui| {
-              ui.label("From ");
-              ui.label(f1.colored_name());
-              ui.label("To ");
-              ui.label(f2.colored_name());
-              match self.db.get_purge_values(f1.id, f2.id) {
-                Ok(v) => {
-                  ui.visuals_mut().override_text_color = Some(egui::Color32::BLACK);
-                  // ui.label(format!("Existing Value: {}", v));
-                  ui.label(format!("({})", v));
+                if self.enter_purge.picker1.selected != self.enter_purge.prev1 {
+                    self.enter_purge.prev1 = self.enter_purge.picker1.selected.clone();
+                    self.enter_purge.purge1.clear();
+                    self.enter_purge.purge2.clear();
                 }
-                _ => {}
-              }
-              // let edit = egui::TextEdit::singleline(&mut self.enter_purge.purge1).clip_text(true);
-              // let resp = ui.add(edit);
-              let resp = ui.text_edit_singleline(&mut self.enter_purge.purge1);
-            });
-
-            ui.horizontal(|ui| {
-              ui.label("From ");
-              ui.label(f2.colored_name());
-              ui.label("To ");
-              ui.label(f1.colored_name());
-              match self.db.get_purge_values(f2.id, f1.id) {
-                Ok(v) => {
-                  ui.visuals_mut().override_text_color = Some(egui::Color32::BLACK);
-                  // ui.label(format!("Existing Value: {}", v));
-                  ui.label(format!("({})", v));
+                if self.enter_purge.picker2.selected != self.enter_purge.prev2 {
+                    self.enter_purge.prev2 = self.enter_purge.picker2.selected.clone();
+                    self.enter_purge.purge1.clear();
+                    self.enter_purge.purge2.clear();
                 }
-                _ => {}
-              }
-              let resp = ui.text_edit_singleline(&mut self.enter_purge.purge2);
-            });
 
-            if ui.button("Save Vaules").clicked() {
-              if let Ok(p) = self.enter_purge.purge1.parse::<u32>() {
-                self.db.set_purge_values(f1.id, f2.id, p).unwrap();
-              }
-              if let Ok(p) = self.enter_purge.purge2.parse::<u32>() {
-                self.db.set_purge_values(f2.id, f1.id, p).unwrap();
-              }
-            }
-          }
-          _ => {}
-        }
-      });
-  }
+                // if resp1.changed() || resp2.changed() {
+                //   self.enter_purge.purge1.clear();
+                //   self.enter_purge.purge2.clear();
+                // }
+
+                ui.separator();
+
+                match (
+                    &self.enter_purge.picker1.selected,
+                    &self.enter_purge.picker2.selected,
+                ) {
+                    (Some(f1), Some(f2)) => {
+                        if f1 == f2 {
+                            return;
+                        }
+                        ui.horizontal(|ui| {
+                            ui.label("From ");
+                            ui.label(f1.colored_name());
+                            ui.label("To ");
+                            ui.label(f2.colored_name());
+                            match self.db.get_purge_values(f1.id, f2.id) {
+                                Ok(v) => {
+                                    ui.visuals_mut().override_text_color =
+                                        Some(egui::Color32::BLACK);
+                                    // ui.label(format!("Existing Value: {}", v));
+                                    ui.label(format!("({})", v));
+                                }
+                                _ => {}
+                            }
+                            // let edit = egui::TextEdit::singleline(&mut self.enter_purge.purge1).clip_text(true);
+                            // let resp = ui.add(edit);
+                            let resp = ui.text_edit_singleline(&mut self.enter_purge.purge1);
+                        });
+
+                        ui.horizontal(|ui| {
+                            ui.label("From ");
+                            ui.label(f2.colored_name());
+                            ui.label("To ");
+                            ui.label(f1.colored_name());
+                            match self.db.get_purge_values(f2.id, f1.id) {
+                                Ok(v) => {
+                                    ui.visuals_mut().override_text_color =
+                                        Some(egui::Color32::BLACK);
+                                    // ui.label(format!("Existing Value: {}", v));
+                                    ui.label(format!("({})", v));
+                                }
+                                _ => {}
+                            }
+                            let resp = ui.text_edit_singleline(&mut self.enter_purge.purge2);
+                        });
+
+                        if ui.button("Save Vaules").clicked() {
+                            if let Ok(p) = self.enter_purge.purge1.parse::<u32>() {
+                                let p = match (
+                                    self.filament_grid.use_multiplier,
+                                    self.filament_grid.use_offset,
+                                ) {
+                                    (true, true) => p,
+                                    (true, false) => {
+                                        (p as f32 * self.filament_grid.multiplier) as u32
+                                    }
+                                    (false, true) => p + self.filament_grid.offset,
+                                    (false, false) => p,
+                                };
+                                self.db.set_purge_values(f1.id, f2.id, p).unwrap();
+                            }
+                            if let Ok(p) = self.enter_purge.purge2.parse::<u32>() {
+                                let p = match (
+                                    self.filament_grid.use_multiplier,
+                                    self.filament_grid.use_offset,
+                                ) {
+                                    (true, true) => p,
+                                    (true, false) => {
+                                        (p as f32 * self.filament_grid.multiplier) as u32
+                                    }
+                                    (false, true) => p + self.filament_grid.offset,
+                                    (false, false) => p,
+                                };
+                                self.db.set_purge_values(f2.id, f1.id, p).unwrap();
+                            }
+                        }
+                    }
+                    _ => {}
+                }
+
+                ui.separator();
+                ui.horizontal(|ui| {
+                    ui.checkbox(&mut self.filament_grid.use_multiplier, "Use multiplier");
+                    let drag = egui::DragValue::new(&mut self.filament_grid.multiplier)
+                        .update_while_editing(false)
+                        .max_decimals(3);
+                    ui.add(drag);
+                });
+                ui.horizontal(|ui| {
+                    ui.checkbox(&mut self.filament_grid.use_offset, "Use offset");
+                    let drag = egui::DragValue::new(&mut self.filament_grid.offset)
+                        .update_while_editing(false)
+                        .max_decimals(0);
+                    ui.add(drag);
+                });
+            });
+    }
 }
--- src/gui/filament_grid.rs
+++ src/gui/filament_grid.rs
@@ -4,179 +4,235 @@ use super::{filament_picker::FilamentPicker, text_val::ValText, App};
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 pub struct FilamentGrid {
-  pickers: [FilamentPicker; 16],
-  num_filaments: usize,
-  // cells: [[ValText<u32>; 16]; 16],
+    pickers: [FilamentPicker; 16],
+    num_filaments: usize,
+    // cells: [[valtext<u32>; 16]; 16],
+    pub multiplier: f32,
+    pub use_multiplier: bool,
+    pub offset: u32,
+    pub use_offset: bool,
 }
 
 impl Default for FilamentGrid {
-  fn default() -> Self {
-    // let cells = std::array::from_fn(|_| {
-    //   std::array::from_fn(|_| {
-    //     ValText::with_validator(|text| text.parse::<u32>().ok().filter(|&n| n < 1000))
-    //   })
-    // });
+    fn default() -> Self {
+        // let cells = std::array::from_fn(|_| {
+        //   std::array::from_fn(|_| {
+        //     ValText::with_validator(|text| text.parse::<u32>().ok().filter(|&n| n < 1000))
+        //   })
+        // });
 
-    Self {
-      pickers: Default::default(),
-      num_filaments: 4,
-      // cells,
+        Self {
+            pickers: Default::default(),
+            num_filaments: 4,
+            multiplier: 1.0,
+            use_multiplier: false,
+            offset: 0,
+            use_offset: false,
+            // cells,
+        }
     }
-  }
 }
 
 impl App {
-  pub fn show_filament_grid(&mut self, ui: &mut egui::Ui) {
-    let filaments = self.db.get_all_filaments().unwrap();
+    pub fn show_filament_grid(&mut self, ui: &mut egui::Ui) {
+        let filaments = self.db.get_all_filaments().unwrap();
 
-    egui::Frame::none()
-      .outer_margin(5.)
-      .inner_margin(5.)
-      .show(ui, |ui| {
-        ui.visuals_mut().override_text_color = Some(egui::Color32::BLACK);
-        ui.horizontal(|ui| {
-          if ui.button("Clear").clicked() {
-            for p in self.filament_grid.pickers.iter_mut() {
-              p.reset();
-            }
-          }
-          if ui.button("White + Black").clicked() {
-            let f = self.db.get_filament(2).unwrap();
-            self.filament_grid.pickers[1].selected = Some(f);
-            let f = self.db.get_filament(1).unwrap();
-            self.filament_grid.pickers[2].selected = Some(f);
-          }
-        });
-
-        ui.horizontal(|ui| {
-          if ui.button("+").clicked() {
-            if self.filament_grid.num_filaments < 16 {
-              self.filament_grid.num_filaments += 1;
-            }
-          }
-          ui.label(&format!("{}", self.filament_grid.num_filaments));
-          if ui.button("-").clicked() {
-            if self.filament_grid.num_filaments > 2 {
-              self.filament_grid.num_filaments -= 1;
-            }
-          }
-        });
-
-        let mut any_changed = false;
-
-        TableBuilder::new(ui)
-          .striped(true)
-          // .column(Column::auto().at_least(45.))
-          .column(Column::auto().at_least(250.))
-          .columns(Column::exact(40.), self.filament_grid.num_filaments)
-          .header(50., |mut header| {
-            header.col(|ui| {});
-
-            for f in self.filament_grid.pickers[..self.filament_grid.num_filaments].iter() {
-              if let Some(f) = &f.selected {
-                header.col(|ui| {
-                  ui.label(f.colored_box(true));
-                });
-              } else {
-                header.col(|ui| {});
-              }
-            }
-          })
-          .body(|mut body| {
-            for from_id in 0..self.filament_grid.num_filaments {
-              body.row(20., |mut row| {
-                row.col(|ui| {
-                  self.filament_grid.pickers[from_id].filament_picker(Some(400.), &filaments, ui);
-                });
-
-                for (to_id, to) in self.filament_grid.pickers[..self.filament_grid.num_filaments]
-                  .iter()
-                  .enumerate()
-                {
-                  row.col(|ui| {
-                    if from_id == to_id {
-                      egui::Frame::none()
-                        .fill(egui::Color32::from_gray(32))
-                        .show(ui, |ui| {
-                          ui.allocate_space(ui.available_size());
-                        });
-                    }
-
-                    match (
-                      &self.filament_grid.pickers[from_id].selected,
-                      &self.filament_grid.pickers[to_id].selected,
-                    ) {
-                      (Some(from), Some(to)) => {
-                        if let Ok(purge) = self.db.get_purge_values(from.id, to.id) {
-                          ui.label(format!("{purge}"));
+        egui::Frame::none()
+            .outer_margin(5.)
+            .inner_margin(5.)
+            .show(ui, |ui| {
+                ui.visuals_mut().override_text_color = Some(egui::Color32::BLACK);
+                ui.horizontal(|ui| {
+                    if ui.button("Clear").clicked() {
+                        for p in self.filament_grid.pickers.iter_mut() {
+                            p.reset();
                         }
-                      }
-                      _ => {}
                     }
+                    if ui.button("White + Black").clicked() {
+                        let f = self.db.get_filament(2).unwrap();
+                        self.filament_grid.pickers[1].selected = Some(f);
+                        let f = self.db.get_filament(1).unwrap();
+                        self.filament_grid.pickers[2].selected = Some(f);
+                    }
+                });
 
-                    // let cell = &mut self.filament_grid.cells[from_id][to_id];
-                    // let edit = if !cell.is_valid() {
-                    //   egui::TextEdit::singleline(cell)
-                    //     .clip_text(false)
-                    //     .text_color(egui::Color32::RED)
-                    // } else {
-                    //   egui::TextEdit::singleline(cell)
-                    //     .clip_text(false)
-                    // };
+                ui.horizontal(|ui| {
+                    if ui.button("+").clicked() {
+                        if self.filament_grid.num_filaments < 16 {
+                            self.filament_grid.num_filaments += 1;
+                        }
+                    }
+                    ui.label(&format!("{}", self.filament_grid.num_filaments));
+                    if ui.button("-").clicked() {
+                        if self.filament_grid.num_filaments > 2 {
+                            self.filament_grid.num_filaments -= 1;
+                        }
+                    }
+                });
 
-                    // if ui.add(edit).changed() {
-                    // }
+                let mut any_changed = false;
 
-                    // let resp = ui.text_edit_singleline(&mut self.filament_grid.cells[from_id][to_id]);
-                  });
+                TableBuilder::new(ui)
+                    .striped(true)
+                    // .column(Column::auto().at_least(45.))
+                    .column(Column::auto().at_least(250.))
+                    .columns(Column::exact(40.), self.filament_grid.num_filaments)
+                    .header(50., |mut header| {
+                        header.col(|ui| {});
+
+                        for f in
+                            self.filament_grid.pickers[..self.filament_grid.num_filaments].iter()
+                        {
+                            if let Some(f) = &f.selected {
+                                header.col(|ui| {
+                                    ui.label(f.colored_box(true));
+                                });
+                            } else {
+                                header.col(|ui| {});
+                            }
+                        }
+                    })
+                    .body(|mut body| {
+                        for from_id in 0..self.filament_grid.num_filaments {
+                            body.row(20., |mut row| {
+                                row.col(|ui| {
+                                    self.filament_grid.pickers[from_id].filament_picker(
+                                        Some(400.),
+                                        &filaments,
+                                        ui,
+                                    );
+                                });
+
+                                for (to_id, to) in self.filament_grid.pickers
+                                    [..self.filament_grid.num_filaments]
+                                    .iter()
+                                    .enumerate()
+                                {
+                                    row.col(|ui| {
+                                        if from_id == to_id {
+                                            egui::Frame::none()
+                                                .fill(egui::Color32::from_gray(32))
+                                                .show(ui, |ui| {
+                                                    ui.allocate_space(ui.available_size());
+                                                });
+                                        }
+
+                                        match (
+                                            &self.filament_grid.pickers[from_id].selected,
+                                            &self.filament_grid.pickers[to_id].selected,
+                                        ) {
+                                            (Some(from), Some(to)) => {
+                                                if let Ok(purge) =
+                                                    self.db.get_purge_values(from.id, to.id)
+                                                {
+                                                    let purge = match (
+                                                        self.filament_grid.use_multiplier,
+                                                        self.filament_grid.use_offset,
+                                                    ) {
+                                                        (true, true) => purge,
+                                                        (true, false) => {
+                                                            (purge as f32
+                                                                * self.filament_grid.multiplier)
+                                                                as u32
+                                                        }
+                                                        (false, true) => {
+                                                            purge + self.filament_grid.offset
+                                                        }
+                                                        (false, false) => purge,
+                                                    };
+                                                    ui.label(format!("{purge}"));
+                                                }
+                                            }
+                                            _ => {}
+                                        }
+
+                                        // let cell = &mut self.filament_grid.cells[from_id][to_id];
+                                        // let edit = if !cell.is_valid() {
+                                        //   egui::TextEdit::singleline(cell)
+                                        //     .clip_text(false)
+                                        //     .text_color(egui::Color32::RED)
+                                        // } else {
+                                        //   egui::TextEdit::singleline(cell)
+                                        //     .clip_text(false)
+                                        // };
+
+                                        // if ui.add(edit).changed() {
+                                        // }
+
+                                        // let resp = ui.text_edit_singleline(&mut self.filament_grid.cells[from_id][to_id]);
+                                    });
+                                }
+                            });
+                        }
+                    });
+
+                if ui.button("send to orca").clicked() {
+                    self.send_purge_values(self.filament_grid.num_filaments)
+                        .unwrap();
                 }
-              });
-            }
-          });
 
-        if ui.button("send to orca").clicked() {
-          self
-            .send_purge_values(self.filament_grid.num_filaments)
-            .unwrap();
-        }
-      });
-  }
-
-  fn send_purge_values(&self, num_filaments: usize) -> anyhow::Result<()> {
-    // if num_filaments != 4 {
-    //   panic!("num_filaments TODO");
-    // }
-
-    crate::input_sender::alt_tab()?;
-    std::thread::sleep(std::time::Duration::from_millis(400));
-    // eprintln!("alt-tab");
-
-    for from_id in 0..num_filaments {
-      let Some(from) = &self.filament_grid.pickers[from_id].selected else {
-        panic!("missing from");
-      };
-      for to_id in 0..num_filaments {
-        let Some(to) = &self.filament_grid.pickers[to_id].selected else {
-          panic!("missing to");
-        };
-        if from_id == to_id {
-          continue;
-        }
-        if let Ok(purge) = self.db.get_purge_values(from.id, to.id) {
-          // eprintln!("sending purge = {:?}", purge);
-          crate::input_sender::send_number(purge, true)?;
-        } else {
-          crate::input_sender::send_number(999, true)?;
-        }
-      }
-      for _ in 0..4 - num_filaments {
-        // eprintln!("tab");
-        crate::input_sender::tab()?;
-      }
-      // eprintln!("tab");
-      crate::input_sender::tab()?;
+                ui.separator();
+                ui.horizontal(|ui| {
+                    ui.checkbox(&mut self.filament_grid.use_multiplier, "Use multiplier");
+                    let drag = egui::DragValue::new(&mut self.filament_grid.multiplier)
+                        .update_while_editing(false)
+                        .max_decimals(3);
+                    ui.add(drag);
+                });
+                ui.horizontal(|ui| {
+                    ui.checkbox(&mut self.filament_grid.use_offset, "Use offset");
+                    let drag = egui::DragValue::new(&mut self.filament_grid.offset)
+                        .update_while_editing(false)
+                        .max_decimals(0);
+                    ui.add(drag);
+                });
+            });
     }
 
-    Ok(())
-  }
+    fn send_purge_values(&self, num_filaments: usize) -> anyhow::Result<()> {
+        // if num_filaments != 4 {
+        //   panic!("num_filaments TODO");
+        // }
+
+        crate::input_sender::alt_tab()?;
+        std::thread::sleep(std::time::Duration::from_millis(400));
+        // eprintln!("alt-tab");
+
+        for from_id in 0..num_filaments {
+            let Some(from) = &self.filament_grid.pickers[from_id].selected else {
+                panic!("missing from");
+            };
+            for to_id in 0..num_filaments {
+                let Some(to) = &self.filament_grid.pickers[to_id].selected else {
+                    panic!("missing to");
+                };
+                if from_id == to_id {
+                    continue;
+                }
+                if let Ok(p) = self.db.get_purge_values(from.id, to.id) {
+                    let purge = match (
+                        self.filament_grid.use_multiplier,
+                        self.filament_grid.use_offset,
+                    ) {
+                        (true, true) => p,
+                        (true, false) => (p as f32 * self.filament_grid.multiplier) as u32,
+                        (false, true) => p + self.filament_grid.offset,
+                        (false, false) => p,
+                    };
+                    // eprintln!("sending purge = {:?}", purge);
+                    crate::input_sender::send_number(purge, true)?;
+                } else {
+                    crate::input_sender::send_number(999, true)?;
+                }
+            }
+            for _ in 0..4 - num_filaments {
+                // eprintln!("tab");
+                crate::input_sender::tab()?;
+            }
+            // eprintln!("tab");
+            crate::input_sender::tab()?;
+        }
+
+        Ok(())
+    }
 }
